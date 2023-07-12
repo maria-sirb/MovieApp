45c0117b799fe89c6a0c32321788f705878f161b
maria-sirb <sirbmariafelicia@gmail.com>
2023-07-12 17:42:50 +0300

    manage menu links based on user logged in or not

--- MovieAppFrontend/src/app/menu/menu.component.ts
+++ MovieAppFrontend/src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Renderer2 } from '@angular/core';
+import { AuthenticationService } from '../shared/services/authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,18 +11,26 @@ import { Renderer2 } from '@angular/core';
 export class MenuComponent {
 
   activeUrl : string = "";
-  constructor(private router : Router, private renderer : Renderer2){
+  isLoggedIn : boolean = false;
+  constructor(private authenticationService : AuthenticationService, private router : Router, private renderer : Renderer2){
+    
+  }
 
+  ngOnInit() : void {
+    this.isLoggedIn = this.authenticationService.isLoggedIn();
   }
 
   onClick(event : Event): void {
     //this.activeUrl = this.router.url;
     console.log(event.target);
     console.log(this.activeUrl);
-   
-    
-   
-   
+  }
+
+  logout(){
+    this.authenticationService.logout();
+    this.router.navigate(['']).then(() => {
+      window.location.reload();
+    })
   }
 
 }
