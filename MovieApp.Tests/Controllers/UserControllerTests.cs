01c8e58404ef8daa45091d0a2203efd492569558
maria-sirb <sirbmariafelicia@gmail.com>
2023-08-18 15:19:37 +0300

    modify tests

--- MovieApp.Tests/Controllers/UserControllerTests.cs
+++ MovieApp.Tests/Controllers/UserControllerTests.cs
@@ -9,6 +9,7 @@ using MovieAppAPI.Dtos;
 using MovieAppAPI.Helper;
 using MovieAppAPI.Interfaces;
 using MovieAppAPI.Models;
+using MovieAppAPI.UtilityService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,7 @@ namespace MovieApp.Tests.Controllers
         private readonly IMapper _mapper;
         private readonly IWebHostEnvironment _hostEnvironment;
         private readonly IPasswordHasher _passwordHasher;
+        private readonly IEmailService _emailService;
 
         public UserControllerTests()
         {
@@ -30,6 +32,7 @@ namespace MovieApp.Tests.Controllers
             _mapper = A.Fake<IMapper>();
             _hostEnvironment = A.Fake<IWebHostEnvironment>();
             _passwordHasher = A.Fake<IPasswordHasher>();
+            _emailService = A.Fake<IEmailService>();
         }
 
         [Fact]
@@ -45,7 +48,7 @@ namespace MovieApp.Tests.Controllers
             //A.CallTo(() => _userRepository.HashUserPassword(user));
             A.CallTo(() => _passwordHasher.HashPassword(user.Password)).Returns("hashed_password");
             A.CallTo(() => _userRepository.CreateUser(user)).Returns(true);
-            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment);
+            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment, _emailService);
 
             //Act
             var result = controller.RegisterUser(userCreate);
@@ -63,7 +66,7 @@ namespace MovieApp.Tests.Controllers
             var user = A.Fake<User>();
             A.CallTo(() => _userRepository.UserExists(userCreate.Email)).Returns(false);
             A.CallTo(() => _userRepository.UsernameExists(userCreate.Username)).Returns(true);
-            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment);
+            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment, _emailService);
 
             //Act
             var result = controller.RegisterUser(userCreate);
@@ -82,7 +85,7 @@ namespace MovieApp.Tests.Controllers
             A.CallTo(() => _userRepository.UserExists(userCreate.Email)).Returns(false);
             A.CallTo(() => _userRepository.UsernameExists(userCreate.Username)).Returns(false);
             A.CallTo(() => _userRepository.CheckPasswordStrength(userCreate.Password)).Returns("Password not strong enough.");
-            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment);
+            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment, _emailService);
 
             //Act
             var result = controller.RegisterUser(userCreate);
@@ -98,10 +101,10 @@ namespace MovieApp.Tests.Controllers
             //Arrange
             var userVerify = A.Fake<UserDto>();
             var user = A.Fake<User>();
-            A.CallTo(() => _userRepository.UserExists(userVerify.Email, userVerify.Password)).Returns(true);
-            A.CallTo(() => _mapper.Map<User>(userVerify)).Returns(user);
+            A.CallTo(() => _userRepository.GetUserByEmail(userVerify.Email)).Returns(user);
+            A.CallTo(() => _passwordHasher.VerifyPassword(userVerify.Password, user.Password)).Returns(true);
             A.CallTo(() => _userRepository.CreateJwt(user));
-            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment);
+            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment, _emailService);
 
             //Act
             var result = controller.AuthenticateUser(userVerify);
@@ -117,8 +120,9 @@ namespace MovieApp.Tests.Controllers
             //Arrange
             var userVerify = A.Fake<UserDto>();
             var user = A.Fake<User>();
-            A.CallTo(() => _userRepository.UserExists(userVerify.Email, userVerify.Password)).Returns(false);
-            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment);
+            A.CallTo(() => _userRepository.GetUserByEmail(userVerify.Email)).Returns(user);
+            A.CallTo(() => _passwordHasher.VerifyPassword(userVerify.Password, user.Password)).Returns(false);
+            var controller = new UserController(_userRepository, _mapper, _passwordHasher, _hostEnvironment, _emailService);
 
             //Act
             var result = controller.AuthenticateUser(userVerify);
