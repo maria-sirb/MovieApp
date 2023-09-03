871e0a6b786b4827f5d1c28c74692c770d8cf123
maria-sirb <sirbmariafelicia@gmail.com>
2023-09-03 18:31:00 +0300

    add role based authorization

--- MovieAppFrontend/src/app/shared/guards/auth.guard.ts
+++ MovieAppFrontend/src/app/shared/guards/auth.guard.ts
@@ -2,20 +2,34 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
+import { UserStoreService } from '../services/user-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authenticationService : AuthenticationService, private router: Router){
+  constructor(private authenticationService : AuthenticationService, private userStoreService : UserStoreService, private router: Router){
 
   }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
     if(this.authenticationService.isLoggedIn())
-      return true;
+    {  
+      this.userStoreService.getRoleFromStore().subscribe(res => {
+        const userRole = res || this.authenticationService.getRoleFromToken(); 
+        const { roles }  = route.data;
+        if(roles && !roles.includes(userRole))
+        {
+          this.router.navigate(['']);
+          return false;
+        }
+        return true;
+    })
+    return true;
+  }
     else
     { 
       this.router.navigate(['login']);
