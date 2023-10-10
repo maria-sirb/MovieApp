474b0b1d27761b0832c6c5635cebe6b641fc4bde
maria-sirb <sirbmariafelicia@gmail.com>
2023-10-10 18:31:06 +0300

    implement debouncing for the search input

--- MovieAppFrontend/src/app/search/search.component.ts
+++ MovieAppFrontend/src/app/search/search.component.ts
@@ -9,26 +9,46 @@ import { DefaultPhoto } from '../shared/functions/default-photos';
 import { UserStoreService } from '../shared/services/user-store.service';
 import { AuthenticationService } from '../shared/services/authentication.service';
 import { WatchlistService } from '../shared/services/watchlist.service';
+import { BehaviorSubject, Subject, combineLatest, debounceTime, distinct, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent{
+export class SearchComponent implements OnInit{
 
   movies? : Movie[];
   actors? : Actor[];
   directors? : Director[];
   dp = new DefaultPhoto();
+  inputSubject = new Subject<string>();
+  noResults = false;
 
   constructor(private movieService : MovieService, private actorService: ActorsService, private directorService : DirectorService){}
-  Search (input : string)
-  {
-    this.movieService.searchMovies(input).subscribe(movies => this.movies = movies);
-    this.actorService.searchActors(input).subscribe(actors => this.actors = actors);
-    this.directorService.searchDirectors(input).subscribe(directors => this.directors = directors);
-  
+  ngOnInit(): void {
+     this.inputSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(input => {
+        return combineLatest([
+          this.movieService.searchMovies(input),
+          this.actorService.searchActors(input),
+          this.directorService.searchDirectors(input)
+        ])
+      })
+     ).subscribe(([movies, actors, directors]) => {
+      this.movies = movies;
+      this.actors = actors;
+      this.directors = directors;
+      if(!movies.length && !actors.length && !directors.length)
+        this.noResults = true;
+    });
   }
   
+  Search (input : string)
+  {
+    this.inputSubject.next(input);
+    this.noResults = false;
+  }
 }
